ff7a20293b45dba396a76e9687a75d39c2cae786
btpink-seo <btpink.seo@gmail.com>
2021-03-28 13:16:48 +0900

    터치 드래그

--- client/src/hooks/useDraggable.tsx
+++ client/src/hooks/useDraggable.tsx
@@ -12,36 +12,59 @@ const useDraggable = (
 
   const dragElement = () => {
     movePartRef.current.onmousedown = dragMouseDown;
+    movePartRef.current.ontouchstart = dragTouchDown;
   };
 
   const dragMouseDown = (e: MouseEvent) => {
-    e = e || window.event;
-    e.preventDefault();
+    const event = e || window.event;
+    event.preventDefault();
     // get the mouse cursor position at startup:
-    pos3.current = e.clientX;
-    pos4.current = e.clientY;
+    pos3.current = event.clientX;
+    pos4.current = event.clientY;
     document.onmouseup = closeDragElement;
     // call a const whenever the cursor moves:
-    document.onmousemove = elementDrag;
+    document.onmousemove = elementDragMouse;
   };
 
-  const elementDrag = (e: MouseEvent) => {
-    e = e || window.event;
+  const dragTouchDown = (e: TouchEvent) => {
     e.preventDefault();
+    const event = e.targetTouches[0];
+    // get the mouse cursor position at startup:
+    pos3.current = event.clientX;
+    pos4.current = event.clientY;
+    document.ontouchmove = elementDragTouch;
+    document.ontouchend = closeDragElement;
+  };
+
+  const elementDragMouse = (e: MouseEvent) => {
+    e.preventDefault();
+    const event = e || window.event;
+    calculateCoordinate(event.clientX, event.clientY);
+  };
+
+  const elementDragTouch = (e: TouchEvent) => {
+    e.preventDefault();
+    const touch = e.targetTouches[0];
+    calculateCoordinate(touch.clientX, touch.clientY);
+  };
+
+  const calculateCoordinate = (x: number, y: number) => {
     // calculate the new cursor position:
-    pos1.current = pos3.current - e.clientX;
-    pos2.current = pos4.current - e.clientY;
-    pos3.current = e.clientX;
-    pos4.current = e.clientY;
+    pos1.current = pos3.current - x;
+    pos2.current = pos4.current - y;
+    pos3.current = x;
+    pos4.current = y;
     // set the element's new position:
     mainRef.current.style.top = `${mainRef.current.offsetTop - pos2.current}px`;
     mainRef.current.style.left = `${mainRef.current.offsetLeft - pos1.current}px`;
+
   };
 
   const closeDragElement = () => {
-    // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
+    document.ontouchmove = null;
+    document.ontouchend = null;
   };
 
   return { dragElement };

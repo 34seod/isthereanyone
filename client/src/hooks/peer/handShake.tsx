74e86fee84326d071d0db18d0086ae2eddbe574c
btpink-seo <btpink.seo@gmail.com>
2021-04-04 11:21:18 +0900

    turn 서버 원복

--- client/src/hooks/peer/handShake.tsx
+++ client/src/hooks/peer/handShake.tsx
@@ -33,10 +33,10 @@ const pcConfig = {
     { 'urls':'stun:stun.l.google.com:19302' },
     {
       'urls': [
-        process.env.REACT_APP_TURN_SERVER || 'turn:numb.viagenie.ca'
+        'turn:numb.viagenie.ca'
       ],
-      'username': process.env.REACT_APP_TURN_USERNAME || 'webrtc@live.com',
-      'credential': process.env.REACT_APP_TURN_CREDENTIAL || 'muazkh'
+      'username': 'webrtc@live.com',
+      'credential': 'muazkh'
     }
   ]
 };
